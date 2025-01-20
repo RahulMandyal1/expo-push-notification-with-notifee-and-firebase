c50fc8dc461156d39a2c9e609bf704daf6ca72ee
Rahul Mandyal <rahulmandyal079@gmail.com>
2025-01-20 23:33:46 +0530

    UI setup WIP

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,8 +1,37 @@
 import { Image, StyleSheet, Platform } from 'react-native';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
+import notifee from '@notifee/react-native';
+
 
 export default function HomeScreen() {
+
+     async function onDisplayNotification() {
+    // Request permissions (required for iOS)
+    await notifee.requestPermission()
+
+    // Create a channel (required for Android)
+    const channelId = await notifee.createChannel({
+      id: 'default',
+      name: 'Default Channel',
+    });
+
+    // Display a notification
+    await notifee.displayNotification({
+      title: 'Notification Title',
+      body: 'Main body content of the notification',
+      android: {
+        channelId,
+        smallIcon: 'name-of-a-small-icon', // optional, defaults to 'ic_launcher'.
+        // pressAction is needed if you want the notification to open the app when pressed
+        pressAction: {
+          id: 'default',
+        },
+      },
+    });
+  }
+
+
   return (
     <ThemedView>
       <ThemedText>
